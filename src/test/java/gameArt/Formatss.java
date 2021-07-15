e1ecea181e8485dde1cf47d9db507241175d7341
SemihTut <yalaantv@gmail.com>
2021-07-15 10:40:21 +0300

    String.format

--- src/test/java/gameArt/Formatss.java
+++ src/test/java/gameArt/Formatss.java
@@ -5,6 +5,10 @@ public class Formatss {
 
     public static void main(String[] args) {
 
+        String str = "My name is %s";
+        String name = "Semih";
+        System.out.println(String.format(str,name));
+
         String semih = String.format("My name is %s", "semih");
         System.out.println("semih = " + semih);
 
